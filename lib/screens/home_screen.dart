4a66e59ce88a6722103c20abc557eb9f5270e003
Punyaphat Kaewkao <punyaphat0010@gmail.com>
2025-07-23 14:54:32 +0700

    Home: add button for link another page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'profile_page.dart';
+import 'dashboard.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,11 +11,52 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Home Screen!',
-          style: TextStyle(fontSize: 20),
-        ),
+
+      body: Column(
+        children: [
+
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Welcome to the Home Screen!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Divider(),
+
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('Go to Profile'),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const BasicProfileScreen()),
+                    );
+                  },
+                ),
+                const Divider(),
+                
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Go to Dashboard'),
+                  trailing: const Icon(Icons.chevron_right),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const DashboardScreen()),
+                    );
+                  },
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
